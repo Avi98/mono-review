7fe2ca803faa5941a2706d273489a7533c36177b
Avinash <avinash.kumar@renegadeinsurance.com>
2023-09-24 13:13:01 +0530

    feat(ui): added illustrations and images

---
+++ apps/web/src/components/logo/index.tsx
@@ -0,0 +1,24 @@
+import { HTMLAttributes } from "react";
+import { ProductIcon } from "../../../illustrations/ic-productIcon";
+import { cn } from "../../utils/classNameMerge";
+
+interface ILogoIcon extends HTMLAttributes<HTMLDivElement> {}
+
+export const LogoIcon = ({ className, ...props }: ILogoIcon) => (
+  <div
+    className={cn(
+      "flex h-10 w-40 cursor-pointer justify-center align-middle",
+      className
+    )}
+    {...props}
+  >
+    <ProductIcon />
+    <div
+      className={
+        "flex items-center font-mono font-bold capitalize text-sky-500"
+      }
+    >
+      Dockmz
+    </div>
+  </div>
+);
