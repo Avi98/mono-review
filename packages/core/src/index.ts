c029aa653511c21c71e51d5a2806a5c108943ef9
Avinash <avinash.kumar@renegadeinsurance.com>
2023-09-27 00:20:16 +0530

    refator(package): removed core

--- packages/core/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { env } from "./utils/env.js";
-import { Instance } from "./instance/index.js";
-import { LunchServer } from "./launchServer/index.js";
-import type { IGitConfig } from "./interface/IGitConfig.js";
-
-export { Instance, LunchServer, env, IGitConfig };
