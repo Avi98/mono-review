8207e3cad63f4faa65c08530110d9154a6a2688d
Avinash <avinash.kumar@renegadeinsurance.com>
2023-12-13 00:33:21 +0530

    feat(web): UI for empty members

--- apps/web/src/app/(private)/dashboard/members/components/MemberList.tsx
+++ apps/web/src/app/(private)/dashboard/members/components/MemberList.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useOrgMembers } from "../../../../../../api/org";
+import { AddMembers } from "../../../../../../illustrations/ic-add-member";
+import { Button } from "../../../../../components/button/Button";
 import { MemberItem } from "./MemberItem";
 
 export const MemberList = () => {
@@ -8,17 +10,35 @@ export const MemberList = () => {
     "8d9bffe4-6d21-422d-8629-7045cde68b77"
   );
 
-  console.log({ orgMembers });
+  if (!orgMembers) {
+    return (
+      <div className="flex h-full w-full flex-col place-items-center  justify-center">
+        <div className="flex flex-col items-center align-middle">
+          <AddMembers height={"20rem"} width={"20rem"} />
+          <p className="py-3 text-3xl">
+            No members found for this organization
+          </p>
+          <p className="text-muted-foreground my-4 text-lg">
+            Please add members
+          </p>
+          <Button type="button">Add members</Button>
+        </div>
+      </div>
+    );
+  }
   return (
-    <ul className="border-border [&>*]:border-border rounded-2xl border-2 [&>*:last-child]:border-none [&>*]:border-b-2">
-      {orgMembers?.map((orgMember) => (
-        <MemberItem
-          fullName={`${orgMember.user_firstName} ${orgMember.user_lastName}`}
-          email={orgMember.user_email}
-          jobTitle={orgMember.org_user_role}
-          role={orgMember.role}
-        />
-      ))}
-    </ul>
+    <div className="container px-10 py-5">
+      <h3 className="py-3 text-lg">All users in Organization</h3>
+      <ul className="border-border [&>*]:border-border rounded-2xl border-2 [&>*:last-child]:border-none [&>*]:border-b-2">
+        {orgMembers?.map((orgMember) => (
+          <MemberItem
+            fullName={`${orgMember.user_firstName} ${orgMember.user_lastName}`}
+            email={orgMember.user_email}
+            jobTitle={orgMember.org_user_role}
+            role={orgMember.role}
+          />
+        ))}
+      </ul>
+    </div>
   );
 };
