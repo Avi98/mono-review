cf7ebfc29c6b4af9d92296ef8159ce24d2a87200
Avinash <avinash.kumar@renegadeinsurance.com>
2023-12-29 01:33:27 +0530

    fix(web): avatar in header

--- apps/web/src/app/(private)/organizations/layout.tsx
+++ apps/web/src/app/(private)/organizations/layout.tsx
@@ -5,11 +5,11 @@ const PrivateRoute = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-screen w-full overflow-hidden">
       <header
-        className={`bg-header/[0.6] border-border fixed right-0 z-10 max-h-12 w-full border-b shadow-md`}
+        className={`bg-header/[0.6] border-border fixed right-0 z-10 flex w-full flex-row  justify-end border-b align-middle shadow-md`}
       >
-        <div className="flex justify-end gap-2 px-5 py-1.5">
+        <div className="flex items-center gap-4 px-5 py-1.5">
           <SignOutButton />
-          <Avatar fullName="Alex Raonsssssss" />
+          <Avatar fullName="Alex b" />
         </div>
       </header>
       <div className="relative top-12 h-full flex-1">{children}</div>
